2c1e8f5ced1e4bafb68981c7d000ebb0fcfc2083
Luci-Lawless <lu.gmmm@gmail.com>
2017-11-06 17:11:14 +0100

    Display list of actors

--- db/schema.rb
+++ db/schema.rb
@@ -10,27 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20171106150646) do
-=======
-ActiveRecord::Schema.define(version: 20171106150004) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "actors", force: :cascade do |t|
     t.string "name"
     t.string "string"
     t.string "nationality"
     t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "name"
     t.integer "duration"
     t.string "poster"
-
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
